389c3d13cae5f94b8f3d9be221155024dd9bc0ed
capabhypsh <abhay02.bluethink@gmail.com>
2023-01-02 20:27:18 +0530

    initial commit

--- nested_dict/ex7.py
+++ nested_dict/ex7.py
@@ -1,3 +1,4 @@
+# filter unique value from 
 test_list = [{'gfg' : 1, 'is' : 2}, {'best' : 1, 'for' : 3}, {'CS' : 2}]
 
 s = set()
@@ -19,3 +20,13 @@ dic={}
 for i in (dic1,dic2,dic3):
   dic.update(i)
 print(dic)
+for i in dic:
+    if i in dic.keys()==1:
+        pass
+        print("key exists")
+
+# short the dict by key
+import operator
+x={'a':1,'b':4,'c':5,'d':2}
+print(dict(sorted(x.items(),key=operator.itemgetter(1))))
+print(x)
