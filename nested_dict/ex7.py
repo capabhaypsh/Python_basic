7ac3c5ca8dfd754655a6ef0427ae7056733e2bc9
capabhypsh <abhay02.bluethink@gmail.com>
2023-01-02 12:42:32 +0530

    initial commit

--- nested_dict/ex7.py
+++ nested_dict/ex7.py
@@ -6,3 +6,16 @@ for dic in test_list:
       s.add(val)
 print(s)
 
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+# static
+dic1.update(dic2)
+dic1.update(dic3)
+print(dic1)
+# dynamic
+dic={}
+for i in (dic1,dic2,dic3):
+  dic.update(i)
+print(dic)
